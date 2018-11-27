d8b06e36090ec45f872a9ceb86a92e49b1bcc264
dressafb <45362435+dressafb@users.noreply.github.com>
2018-11-27 09:14:00 -0200

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ let app = require('./config/server.js');
 
 let port = 3000;
 
-app.listen(port, function(){
+app.listen(process.env.PORT, function(){
+	var host = server.address().address
+  	var port = server.address().port
 	console.log('Servidor rodando com express na porta', port);
 });
